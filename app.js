fdeca4184d5fca9f0028ad42dcaa6f024053c54a
Alberto <betim009@gmail.com>
2025-06-30 13:49:14 -0300

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+for (let index = 0; index < 5; index++) {
+    if (index === 4) {
+        console.log("Encontrei o numero 4")
+    }
+    
+    if (index !== 4) {
+        console.log("Ixi")
+    }    
+
+}
