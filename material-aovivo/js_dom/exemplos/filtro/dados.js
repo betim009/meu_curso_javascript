8088864c27202cdb4bc773bd54efa9cbbcc2e7b2
Alberto <betim009@gmail.com>
2025-08-04 18:01:17 -0300

    update

---
+++ material-aovivo/js_dom/exemplos/filtro/dados.js
@@ -0,0 +1,70 @@
+export const lanches = [
+  {
+    nome: "X-Burguer",
+    preco: 15.0,
+    descricao: "Pão, hambúrguer de carne e queijo.",
+    categoria: "Lanche",
+    url: "https://sachefmio.blob.core.windows.net/fotos/x-tudo-duplo-73523.jpg"
+  },
+  {
+    nome: "X-Salada",
+    preco: 18.0,
+    descricao: "Pão, hambúrguer, queijo, alface e tomate.",
+    categoria: "Lanche",
+    url: "https://sachefmio.blob.core.windows.net/fotos/x-tudo-duplo-73523.jpg"
+  },
+  {
+    nome: "X-Bacon",
+    preco: 20.0,
+    descricao: "Pão, hambúrguer, bacon crocante e queijo.",
+    categoria: "Lanche",
+    url: "https://sachefmio.blob.core.windows.net/fotos/x-tudo-duplo-73523.jpg"
+  },
+  {
+    nome: "Hot Dog",
+    preco: 12.0,
+    descricao: "Pão com salsicha, ketchup, mostarda e batata palha.",
+    categoria: "Lanche",
+    url: "https://sachefmio.blob.core.windows.net/fotos/x-tudo-duplo-73523.jpg"
+  },
+  {
+    nome: "Batata Frita",
+    preco: 10.0,
+    descricao: "Porção média de batata frita crocante.",
+    categoria: "Fritas",
+    url: "https://sachefmio.blob.core.windows.net/fotos/x-tudo-duplo-73523.jpg"
+  },
+];
+
+export const comidasJaponesas = [
+  {
+    nome: "Sushi",
+    preco: 25.0,
+    descricao: "Bolinho de arroz com peixe cru ou outros ingredientes.",
+    categoria: "Japones",
+  },
+  {
+    nome: "Temaki",
+    preco: 20.0,
+    descricao: "Cone de alga recheado com arroz e peixe cru.",
+    categoria: "Japones",
+  },
+  {
+    nome: "Yakissoba",
+    preco: 22.0,
+    descricao: "Macarrão frito com legumes e carne.",
+    categoria: "Massa",
+  },
+  {
+    nome: "Guioza",
+    preco: 18.0,
+    descricao: "Pastelzinho recheado com carne ou legumes, cozido ou frito.",
+    categoria: "Japones",
+  },
+  {
+    nome: "Sashimi",
+    preco: 30.0,
+    descricao: "Fatias finas de peixe cru sem arroz.",
+    categoria: "Japones",
+  },
+];
--- material-aovivo/js_dom/exemplos/filtro/script.js
+++ material-aovivo/js_dom/exemplos/filtro/script.js
@@ -1,74 +1,15 @@
-const lanches = [
-  {
-    nome: "X-Burguer",
-    preco: 15.0,
-    descricao: "Pão, hambúrguer de carne e queijo.",
-    categoria: "Lanche",
-  },
-  {
-    nome: "X-Salada",
-    preco: 18.0,
-    descricao: "Pão, hambúrguer, queijo, alface e tomate.",
-    categoria: "Lanche",
-  },
-  {
-    nome: "X-Bacon",
-    preco: 20.0,
-    descricao: "Pão, hambúrguer, bacon crocante e queijo.",
-    categoria: "Lanche",
-  },
-  {
-    nome: "Hot Dog",
-    preco: 12.0,
-    descricao: "Pão com salsicha, ketchup, mostarda e batata palha.",
-    categoria: "Lanche",
-  },
-  {
-    nome: "Batata Frita",
-    preco: 10.0,
-    descricao: "Porção média de batata frita crocante.",
-    categoria: "Fritas",
-  },
-];
-
-const comidasJaponesas = [
-  {
-    nome: "Sushi",
-    preco: 25.0,
-    descricao: "Bolinho de arroz com peixe cru ou outros ingredientes.",
-    categoria: "Japones",
-  },
-  {
-    nome: "Temaki",
-    preco: 20.0,
-    descricao: "Cone de alga recheado com arroz e peixe cru.",
-    categoria: "Japones",
-  },
-  {
-    nome: "Yakissoba",
-    preco: 22.0,
-    descricao: "Macarrão frito com legumes e carne.",
-    categoria: "Massa",
-  },
-  {
-    nome: "Guioza",
-    preco: 18.0,
-    descricao: "Pastelzinho recheado com carne ou legumes, cozido ou frito.",
-    categoria: "Japones",
-  },
-  {
-    nome: "Sashimi",
-    preco: 30.0,
-    descricao: "Fatias finas de peixe cru sem arroz.",
-    categoria: "Japones",
-  },
-];
+import { comidasJaponesas, lanches } from "./dados.js";
 
 // SELETORES LANCHES
 const divResultadoLanches = document.getElementById("resultado-lanches");
+
 const btnLanches = document.getElementById("categoria-lanches");
 const btnFritas = document.getElementById("categoria-fritas");
 const btnLimpar = document.getElementById("categoria-limpar");
+const btnBuscar = document.getElementById("btn-buscar");
+
+const inputBuscar = document.getElementById("input-buscar");
+const inputBuscarAuto = document.getElementById("input-buscar-auto");
 
 // SELETORES JAPONES
 
@@ -78,12 +19,12 @@ lanches.forEach((element) => {
         <div>
             <h4>${element.nome}</h4>
             <p>${element.preco}</p>
+            <img src="${element.url}" />
         </div>
     `;
 });
 
 // JAPONES INICIAL
-
 
 // EVENTOS LANCHES
 btnFritas.addEventListener("click", () => {
@@ -95,6 +36,7 @@ btnFritas.addEventListener("click", () => {
         <div>
             <h4>${element.nome}</h4>
             <p>${element.preco}</p>
+            <img src="${element.url}" />
         </div>
     `;
   });
@@ -111,6 +53,7 @@ btnLanches.addEventListener("click", function () {
             <div>
                 <h4>${element.nome}</h4>
                 <p>${element.preco}</p>
+                <img src="${element.url}" />
             </div>
         `;
     }
@@ -133,5 +76,54 @@ btnLimpar.addEventListener("click", () => {
   });
 });
 
+btnBuscar.addEventListener("click", () => {
+  const resultadoInput = inputBuscar.value;
+
+  const filtrarResultado = lanches.filter((element) =>
+    element.nome.includes(resultadoInput)
+  );
+
+  console.log(filtrarResultado);
+
+  if (filtrarResultado.length > 0) {
+    divResultadoLanches.innerHTML = "";
+
+    filtrarResultado.forEach((element) => {
+      divResultadoLanches.innerHTML += `
+        <div>
+            <h4>${element.nome}</h4>
+            <p>${element.preco}</p>
+        </div>
+    `;
+    });
+  } else {
+    return alert("Nao foi encontrado nenhum lanche.");
+  }
+});
+
+inputBuscarAuto.addEventListener("input", () => {
+  const resultadoInput = inputBuscarAuto.value;
+
+  if (resultadoInput.length > 2) {
+    const filtrarResultado = lanches.filter((element) =>
+      element.nome.includes(resultadoInput)
+    );
+
+    if (filtrarResultado.length > 0) {
+      divResultadoLanches.innerHTML = "";
+
+      filtrarResultado.forEach((element) => {
+        divResultadoLanches.innerHTML += `
+        <div>
+            <h4>${element.nome}</h4>
+            <p>${element.preco}</p>
+        </div>
+    `;
+      });
+    } else {
+      return alert("Nao foi encontrado nenhum lanche.");
+    }
+  }
+});
 
 // EVENTOS JAPONES
